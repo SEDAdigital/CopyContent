e5c689fd4db93c51f8f52fea4dbce456f85b5984
Romain Tripault <rt@seda.digital>
2022-10-24 15:10:39 +0200

    Better handling of using resource IDs when searching for target/destination resource
    Closes #3

--- core/components/copycontent/processors/searchresource.class.php
+++ core/components/copycontent/processors/searchresource.class.php
@@ -8,17 +8,26 @@ class SearchResource extends modResourceGetListProcessor
     {
         $query = $this->getProperty('query');
         $current = $this->getProperty('current');
-        if (!empty($query)) {
-            $c->where([
-                'pagetitle:LIKE' => "%$query%",
-                'OR:id:LIKE' => "%$query%",
-                'OR:longtitle:LIKE' => "%$query%",
-                'OR:uri:LIKE' => "%$query%",
-            ]);
-        }
+
         if ($current) {
             $c->where([
                 'AND:id:!=' => $current,
+            ]);
+        }
+
+        if (!empty($query)) {
+            if (is_numeric($query)) {
+                $c->where([
+                    'id' => $query,
+                ]);
+
+                return $c;
+            }
+
+            $c->where([
+                'pagetitle:LIKE' => "%$query%",
+                'OR:longtitle:LIKE' => "%$query%",
+                'OR:uri:LIKE' => "%$query%",
             ]);
         }
 
